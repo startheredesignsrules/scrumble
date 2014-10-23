affdeba180d565fe10519ef64a3e41b564e9dea2
Iris* Chamberlain <irisc@venuelabs.com>
2014-10-23 16:44:11 -0700

    Possible fix for compiling issue.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree
-
-//= require frameworks/bootstrap
-
-//= require global
-//= require profile
-//= require scrum
+//= require_tree .
